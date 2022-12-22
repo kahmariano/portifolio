98865341ffa6e2bb9b13b92c6d619f32ecd20bb3
kahmariano <kmariano138@gmail.com>
2022-12-22 08:09:11 -0300

    começando o sidebar

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,26 @@
+export function Home() {
+  return (
+    <>
+      <div>
+        <img src="" alt="imagem karina" />
+        <h1>Karina Mariano</h1>
+        <h3>Futura desenvolvedora</h3>
+      </div>
+      <div>
+        <p>
+          brasil <img src="" alt="icone de localizaçao" />
+        </p>
+        <p>
+          Rocketseat <img src="" alt="icone de predio" />
+        </p>
+        <a href="" src="">
+          kahmariano
+        </a>
+        <a href="">mysite</a>
+        <p>
+          kmariano...@gmail.com <img src="" alt="icone de email" />
+        </p>
+      </div>
+    </>
+  )
+}
